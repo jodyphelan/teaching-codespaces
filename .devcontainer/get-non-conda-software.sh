d3072752d6ec78fa91559b97a62c7541bb6e5b74
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-03-10 14:42:24 +0000

    update image

--- .devcontainer/get-non-conda-software.sh
+++ .devcontainer/get-non-conda-software.sh
@@ -3,3 +3,8 @@ cd ~/git
 git clone https://github.com/jodyphelan/genomics_course.git
 
 bash ~/git/genomics_course/scripts/setup_machine/03_get_non_conda_programs.sh
+
+
+ln -s ~/miniforge3/condabin/mamba ~/bin/mamba
+
+echo 'alias "ca=conda activate"' >> ~/.bashrc
